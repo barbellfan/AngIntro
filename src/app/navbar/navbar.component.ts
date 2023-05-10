bec1744d20e3fa062219923e4582d371005404e1
Chris Allen <gh.84kjuqg365@gmail.com>
2023-05-09 20:38:00 -0700

    Use backticks for the template as well

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  template: '<h1> Navbar Template </h1>',
+  template: `<h1> Navbar Template </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita numquam sunt, debitis facilis error, in minus sed id blanditiis, quisquam dignissimos adipisci ipsum hic nihil quibusdam accusantium corrupti ullam.</p>`,
   styles: [`h1 {
     color: red;
     font-size: 50px
