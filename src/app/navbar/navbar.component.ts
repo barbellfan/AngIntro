147848608696a4f33f8c7f819ad7d1429993c9dc
Chris Allen <gh.84kjuqg365@gmail.com>
2023-05-09 20:36:22 -0700

    Writing the css using backticks

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   template: '<h1> Navbar Template </h1>',
-  styles: ['h1{ color: red }']
+  styles: [`h1 {
+    color: red;
+    font-size: 50px
+  }`]
 })
 
 export class NavbarComponent {
