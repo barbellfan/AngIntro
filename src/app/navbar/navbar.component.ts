b1c8ebae85cb2c6cb1ff931ec43367fb1dd2dab5
Chris Allen <gh.84kjuqg365@gmail.com>
2023-05-09 20:44:58 -0700

    Move template and css to separate files

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,12 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  template: `<h1> Navbar Template </h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita numquam sunt, debitis facilis error, in minus sed id blanditiis, quisquam dignissimos adipisci ipsum hic nihil quibusdam accusantium corrupti ullam.</p>`,
-  styles: [`h1 {
-    color: red;
-    font-size: 50px
-  }`]
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
 
 export class NavbarComponent {
