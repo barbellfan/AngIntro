20411044d2307baf2dc532f0c52e1f73977ce875
Chris Allen <gh.84kjuqg365@gmail.com>
2023-05-20 19:52:15 -0700

    Enter instructor's solution. I forgot so much after not studying for a week.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,23 +8,11 @@ import { PostComponent } from './post/post.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  postFormText: string = '';
-  postDetailsText: string = '';
-  postImageUrl: string = '';
-  postUrl: string = '';
-  myColor: string = 'yellow';
-  isChecked: boolean = false;
-
-  toggleChecked($event: Event) {
-    if (this.isChecked) {
-      this.myColor = 'yellow';
-    } else {
-      this.myColor = 'white';
-    }
-
-    this.isChecked = !this.isChecked;
-  }
+  postTitle: string = '';
+  postDetails: string = '';
+  imageURL: string = '';
+  postURL: string = '';
+  addBackground: boolean = false;
 }
-// finish up the task, then check the solution video
 // sample image link
 // https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg
