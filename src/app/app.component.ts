f566d024f5126f479403329bc00eb3ee2efbf283
Chris Allen <gh.84kjuqg365@gmail.com>
2023-05-22 19:44:14 -0700

    Clear out old sections to start new.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-
-import { PostComponent } from './post/post.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +6,4 @@ import { PostComponent } from './post/post.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  postTitle: string = '';
-  postDetails: string = '';
-  imageURL: string = '';
-  postURL: string = '';
-  addBackground: boolean = false;
 }
-// sample image link
-// https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,9 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PostComponent } from './post/post.component';
-import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    PostComponent,
-    PostListComponent,
   ],
   imports: [
     BrowserModule,
