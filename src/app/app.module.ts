8f8952b33bd88a8035e801005740079affb9bd46
Chris Allen <gh.84kjuqg365@gmail.com>
2023-05-28 14:06:37 -0700

    custom pipe useing the angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppendPipe } from './pipes/append.pipe';
+import { AppendCLIPipe } from './pipes/append-cli.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppendPipe
+    AppendPipe,
+    AppendCLIPipe
   ],
   imports: [
     BrowserModule,
