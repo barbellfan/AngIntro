e70f97660da4f7f34823c02f8bf268c7e20b6ea2
Chris Allen <gh.84kjuqg365@gmail.com>
2023-05-29 13:08:30 -0700

    Remove all old code to start next section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,6 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-  userdetails = {
-    name: "User 1",
-    city: "New York",
-    country: "US"
-  }
-
-  dummyText: string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod modi mollitia ut corporis, quaerat pariatur, adipisci minus tenetur ratione recusandae est totam maxime labore magnam perspiciatis quibusdam necessitatibus natus vel!';
 
   constructor() {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppendPipe } from './pipes/append.pipe';
-import { AppendCLIPipe } from './pipes/append-cli.pipe';
-import { SummaryPipe } from './pipes/summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppendPipe,
-    AppendCLIPipe,
-    SummaryPipe
   ],
   imports: [
     BrowserModule,
