6d21285299da63deb3f17b707352d937b6c8d02d
Chris Allen <gh.84kjuqg365@gmail.com>
2023-06-01 20:56:17 -0700

    Use service Revactor to use post component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/post/post.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { PostService } from '../services/post.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+
+
+export class PostComponent {
+  posts: Array<any>;
+
+  constructor() {
+    let postService = new PostService();
+    this.posts = postService.postList;
+  }
+
+}
