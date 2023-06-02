886f95ccb0edd7a657921662084b97a7679c3cd7
Chris Allen <gh.84kjuqg365@gmail.com>
2023-06-01 21:08:12 -0700

    Add PostService to a different component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
+    PostListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { PostService } from '../services/post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+  providers: [PostService],
+})
+export class PostListComponent {
+  postList: Array<any>;
+  constructor(private postService: PostService) {
+    this.postList =  postService.postList;
+  }
+}
