0b5a10a417bb413d7ffe069350f85a5524ff1f19
Chris Allen <gh.84kjuqg365@gmail.com>
2023-06-10 13:53:22 -0700

    Add reactive validation. it all works now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,17 @@ import { FormControl, NgForm, NgModel, FormGroup, Validators } from '@angular/fo
 export class AppComponent {
 
   form: any;
+  emailRegex: string = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$";
 
   constructor() {
     this.form = new FormGroup({
       fullName: new FormControl("", [Validators.required, Validators.minLength(5)]),
-      email: new FormControl(),
-      address: new FormControl()
+      email: new FormControl('', [
+        Validators.required,
+        //Validators.pattern(this.emailRegex),
+        Validators.email,
+      ]),
+      address: new FormControl('', Validators.required)
     });
   }
   onSubmit(f: NgForm) {
@@ -23,8 +28,19 @@ export class AppComponent {
 
   }
 
+  get fullname() {
+    return this.form.get('fullName');
+  }
+
+  get Email() {
+    return this.form.get('email');
+  }
+
+  get Address() {
+    return this.form.get('address');
+  }
+
   getValue(ff: NgModel) {
     console.log(ff);
-
   }
 }
